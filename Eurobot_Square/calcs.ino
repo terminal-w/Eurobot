b169fa2b5543ee91cd7c624910b7f687e274cd75
terminal-w <29689568+terminal-w@users.noreply.github.com>
2018-03-15 10:52:44 +0000

    Splitting Project into tabs

--- Eurobot_Square/calcs.ino
+++ Eurobot_Square/calcs.ino
@@ -1,1 +1,96 @@
+int enc_target(int distance) {
+  
+  /* takes the required travel distance in mm x10 an converts it to an encoder target*/
+ float den = pi*wheel_dia;
+ float frac = 3600/den;
+ int out = distance * frac;
+
+ #if debug == 1
+    DEBUG.println(distance, DEC); DEBUG.println(den, DEC); DEBUG.println(frac, DEC);
+    DEBUG.print("Encoder Target:");
+    DEBUG.print(out, DEC);
+    DEBUG.println(" degrees");
+ #endif
+ return out;
+}
+void turn(int theta){
+    /* takes two arguments a target angle, theta (degrees x10), and a switch, spot,
+    to determine whether the angle is to describe an arc or a spot turn.
+    executes turn */
+    float distance; //distance to be traveled per in mm
+    distance = (theta/3600)*pi*(track);
+    #if debug == 1
+      DEBUG.print("turn: ");
+      DEBUG.println(distance);
+    #endif 
+    int E2tar = enc_target((int)distance*10);
+    int E1tar = enc_target(-(int)distance*10);
+    SP0 = E1tar; SP1 = E2tar;
+    DriveTo(E1tar, E2tar);
+    return;
+}
+int sweep(int distance, int radius, bool in = 0 ){
+  /* code to allow robot to describe an arc
+     returns the inner & outer arc lengths in mm x10*/
+  int Ri = radius - track/20;
+  int Ro = radius + track/20;
+  int Di = (distance/radius)*Ri;
+  int Do = (distance/radius)*Ro;
+  #if debug == 1
+    DEBUG.print("D(i) = ");
+    DEBUG.print(Di/10, DEC);
+    DEBUG.print(", D(o) = ");
+    DEBUG.print(Do/10, DEC);
+    if(in){DEBUG.println(" Return: D(i)");}
+    else{DEBUG.println(" Return: D(o)");}
+  #endif
+  if(in){return Di;}
+  else{return Do;}
+}
+void target(int distance, int radius) {
+#if debug == 1
+  DEBUG.println("Targeting...");
+#endif
+  bool acw = 0;
+  int E1Tar;
+  int E2Tar;
+  if (radius == 0) {
+    E1Tar = enc_target(distance);
+    E2Tar = E1Tar;
+#if debug == 1
+    DEBUG.print("Straight Line, length: ");
+    DEBUG.println(distance, DEC);
+#endif
+  }
+  else {
+    int Do = sweep(distance, abs(radius));
+    int Di = sweep(distance, abs(radius), 1);
+    int EoTar = enc_target(Do);
+    int EiTar = enc_target(Di);
+#if debug == 1
+    DEBUG.print("Swept Radius, length, radius: ");
+    DEBUG.print(distance, DEC);
+    DEBUG.print(", ");
+    DEBUG.print(radius, DEC);
+#endif
+    if (radius < 0) {
+      E1Tar = EoTar;
+      E2Tar = EiTar;
+#if debug == 1
+      DEBUG.println(" Anti-Clockwise");
+#endif
+    }
+    else {
+      E1Tar = EiTar;
+      E2Tar = EoTar;
+#if debug == 1
+      DEBUG.println(" Clockwise");
+#endif
+    }
+  }
+  SP0 = E1Tar; SP1 = E2Tar;
+  DriveTo(E1Tar, E2Tar);
+    notify();
+  return;
+}
 
