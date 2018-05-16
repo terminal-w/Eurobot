515d4451f842dbde2026d58eadbc24e705225087
terminal-w <29689568+terminal-w@users.noreply.github.com>
2018-05-16 20:20:17 +0100

    going green
    adjusting for hypersensitive us in wp 4

--- Eurobot_Square_PID_i2c/Eurobot_Square_PID_i2c.ino
+++ Eurobot_Square_PID_i2c/Eurobot_Square_PID_i2c.ino
@@ -51,7 +51,7 @@ PID Wheel0(&Input0, &Output0, &SP0, Kp, Ki, Kd, DIRECT);
 PID Wheel1(&Input1, &Output1, &SP1, Kp, Ki, Kd, DIRECT);
 
 #define debug 1   //switch for Software Serial
-#define colour 0 //switch for team (1 is green, 0 is orange)
+#define colour 1 //switch for team (1 is green, 0 is orange)
 
 #define pi 3.1415926 //saves any errors typing
 #define MD25 Wire //I2C MD25
@@ -78,7 +78,7 @@ const int waypoints[wps][6] ={
                                  {1,      850,      0,       0,    0,      0},
                                  {2,    -1000,      0,    -900,    0,      6},
                                  {3,     5840,      0,    -900,    0,   1023},
-                                 {4,    12470,      0,     900,    0,     40},
+                                 {4,    12470,      0,     900,    0,     30},
                                  {5,     4500,      0,    -900,    0,     10},
                                  {6,     6800,      0,       0,    4,      0}
                               };
